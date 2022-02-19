ad65441d97bdd060c9c40199eb2c75981eb63283
Fabritch <99616259+Fabritch@users.noreply.github.com>
2022-02-18 18:34:33 -0600

    Corrección de errores
    Se procedió a corregir los errores perceptibles por flake8

--- Tarea 1/flake8_errors.py
+++ Tarea 1/flake8_errors.py
@@ -1,6 +1,6 @@
 def num_to_str(x):  # La función recibe un número entre 0 y 99
     if type(x) == int:
-        if 0<=x <= 99:
+        if 0 <= x <= 99:
             unidades = {1: "uno", 2: "dos", 3: "tres", 4: "cuatro", 5: "cinco",
                         6: "seis", 7: "siete", 8: "ocho", 9: "nueve"}
             # Se crea un diccionario con las unidades.
@@ -8,14 +8,15 @@ def num_to_str(x):  # La función recibe un número entre 0 y 99
                          4: "cuarenta_y_", 5: "cincuenta_y_", 6: "sesenta_y_",
                          7: "setenta_y_", 8: "ochenta_y_", 9: "noventa_y_"}
             # Se crea un diccionario con las decenas.
-            especiales = {0: "cero", 10: "diez", 11: "once", 12: "doce", 13: "trece",
+            especiales = {0: "cero", 10: "diez", 11: "once", 12: "doce",
+                          13: "trece",
                           14: "catorce", 15: "quince", 20: "veinte",
                           30: "treinta", 40: "cuarenta", 50: "cincuenta",
                           60: "sesenta", 70: "setenta", 80: "ochenta",
                           90: "noventa"}
             # Se crea un diccionario con los números especiales.
             num = especiales.get(x)
-            if num:  #La función valida si el número está en el diccionario de
+            if num:  # La función valida si el número está en el diccionario de
                 # números especiales.
                 return especiales[x]  # Si está en el diccionario de
                 # números especiales, devuelve el nombre del número.
